f608184ce605167cc91118d71bfed376204ce486
snyarko99 <snyarko99@gmail.com>
2025-01-20 11:42:08 -0500

    Setup and confirming things work

---
+++ cvd_explore.R
@@ -0,0 +1,7 @@
+# Stephen Nyarko
+# Project: Steel Excess Capacity
+# Date: 2025-01-20
+# Description: This script is used to explore the CVD and trade protections data from the World Bank
+
+# Load the necessary libraries
+library(tidyverse)
